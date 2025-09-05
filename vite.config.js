23b248ce55a0616d0df61911b9641a29bded8f25
BartekPpr <bartoszpolak.pr@gmail.com>
2025-09-05 21:20:07 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/',
+  base: 'https://bartekppr.github.io/Portfolio/',
 })
+
 
